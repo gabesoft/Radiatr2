b2a695db4f88ce25548bfd4b3f0f3dc029bf69af
Sudhindra Rao [betarelease] <sudhindra.r.rao@gmail.com>
2011-02-09 21:52:21 -0800

    removed javascript resize effects - using more colors instead

--- radiatr.user.js
+++ radiatr.user.js
@@ -16,67 +16,8 @@ function refresh() {
   ping();
   hudson();
   hudsonFull();
-  userFeedback();
-  scale();
   setTimeout(refresh, 3000);
 }
-
-function scale() {
-	var importantItems = $('.failure .subject').length + $('.building .subject').length;
-	var boringItems = $('.success .subject').length + $('.disabled .subject').length;
-	var windowHeight = $(window).height();
-	
-	//percentage of screen to devote to Building and Failed builds:
-	var sizer = 0.2;
-	var precedence = 0.65;
-	var subjectScaleMultiplier = 4;
-	var statusScaleMultiplier = 2;
-	var commentScaleMultiplier = 3;
-	var itemParts = subjectScaleMultiplier + 
-					statusScaleMultiplier +
-					commentScaleMultiplier;
-	
-	
-	if (importantItems !== 0) {
-		var pixelsPerImportant = (windowHeight / (importantItems + boringItems)) * (1 + sizer);
-		var pixelsPerBoring = (windowHeight - (importantItems * pixelsPerImportant)) / boringItems;
-
-		var importantScale = pixelsPerImportant / itemParts;
-		var boringScale = pixelsPerBoring / itemParts;
-
-						
-		$('.success .subject').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (boringScale * subjectScaleMultiplier), fontAdjustIncrement: 1});
-		$('.success .statusInWords').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (boringScale * statusScaleMultiplier), fontAdjustIncrement: 1});
-		$('.success .changeSetComment').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (boringScale * commentScaleMultiplier), fontAdjustIncrement: 1});
-
-					
-		$('.failure .subject').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (importantScale * subjectScaleMultiplier), fontAdjustIncrement: 1});
-		$('.failure .statusInWords').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (importantScale * statusScaleMultiplier), fontAdjustIncrement: 1});
-		$('.failure .changeSetComment').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (importantScale * commentScaleMultiplier), fontAdjustIncrement: 1});
-
-		$('.disabled .subject').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (importantScale * subjectScaleMultiplier), fontAdjustIncrement: 1});
-		$('.disabled .statusInWords').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (importantScale * statusScaleMultiplier), fontAdjustIncrement: 1});
-		$('.disabled .changeSetComment').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (importantScale * commentScaleMultiplier), fontAdjustIncrement: 1});
-					
-		$('.building .subject').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (importantScale * subjectScaleMultiplier), fontAdjustIncrement: 1});
-		$('.building .statusInWords').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (importantScale * statusScaleMultiplier), fontAdjustIncrement: 1});
-		$('.building .changeSetComment').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (importantScale * commentScaleMultiplier), fontAdjustIncrement: 1});
-		
-		
-		$('.building').filter(':not(:animated)').effect("pulsate", {times: 1}, 2000);		
-	
-	} else {
-		
-		pixelsPerItem = (windowHeight) / boringItems;
-		itemScale = pixelsPerItem / itemParts;
-
-		$('.success .subject').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (itemScale * subjectScaleMultiplier), fontAdjustIncrement: 1});
-		$('.success .statusInWords').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (itemScale * statusScaleMultiplier), fontAdjustIncrement: 1});
-		$('.success .changeSetComment').fitTextToHeight({verticallyCentered: false, maxScrollHeight: (itemScale * commentScaleMultiplier), fontAdjustIncrement: 1});
-	}
-}
-
-
 
 function ping() {
   $('.ping').each(function () {
@@ -85,14 +26,14 @@ function ping() {
       url: $('#' + $(this).attr('id') + ' a').attr('href'),
       id: $(this).attr('id'),
       onload: function(response) {
-          if (response.status == 200) {
-              $('#' + this.id).addClass('success').removeClass('failure');
-              $('#' + this.id + ' span.statusInWords').text('is Online');
-          }
-          else {
-              $('#' + this.id).addClass('disabled').removeClass('success');
-              $('#' + this.id + ' span.statusInWords').text('is Offline');
-          }
+        if (response.status == 200) {
+          $('#' + this.id).addClass('success').removeClass('failure');
+          $('#' + this.id + ' span.statusInWords').text('is Online');
+        }
+        else {
+          $('#' + this.id).addClass('disabled').removeClass('success');
+          $('#' + this.id + ' span.statusInWords').text('is Offline');
+        }
       }
     });
    }
@@ -101,9 +42,9 @@ function ping() {
  
 function getClaimObject(json) {
   for(var i = 0, n = json.actions.length; i < n; i++) {
-	 if(json.actions[i].claimed) {
-		return json.actions[i];
-	 }
+   if(json.actions[i].claimed) {
+     return json.actions[i];
+   }
   }
   return null;
 }
@@ -111,83 +52,74 @@ function getClaimObject(json) {
 function hudson() {
   $('.hudson').each(function () {
     GM_xmlhttpRequest({
-     method: 'GET',
-     url: $('#' + $(this).attr('id') + ' a').attr('href') + '/lastBuild/api/json',
-     baseUrl: $('#' + $(this).attr('id') + ' a').attr('href'),
-     id: '#' + $(this).attr('id'),
-     onload: function(response) {
-	   if (response.status === 404) {
-		$(this.id).addClass('disabled');
-	   }
-
-
-	   var status = JSON.parse(response.responseText);
-	  
-	   updateClass(status, $(this.id))
-	   
-			   
-	  
-	 	   
-		var statusInWords = message(status) + '&nbsp;' + duration(status, this.id) + differentialTime(status.timestamp);
-       $(this.id + ' span.statusInWords').html(statusInWords);
-	    var changeSetComment = status.changeSet.items.length > 0 ? status.changeSet.items[0].comment : "Missing Comment!";
-       $(this.id + " span.changeSetComment").html(changeSetComment.substring(0, 140));
-       var claim = getClaimObject(status);
-       if(claim)
-       {
-         $(this.id + " span.claim").html("Claimed by " + claim.claimedBy + " because " + claim.reason);
-       }
-	   if(!claim)
-	   {
-		 $(this.id + " span.claim").css('display', 'hidden');
-
-	   }
-    }
-   });
+      method: 'GET',
+      url: $('#' + $(this).attr('id') + ' a').attr('href') + '/lastBuild/api/json',
+      baseUrl: $('#' + $(this).attr('id') + ' a').attr('href'),
+      id: '#' + $(this).attr('id'),
+      onload: function(response) {
+        if (response.status === 404) {
+          $(this.id).addClass('disabled');
+        }
+        var status = JSON.parse(response.responseText);
+        updateClass(status, $(this.id))
+        var statusInWords = message(status) + '&nbsp;' + duration(status, this.id) + differentialTime(status.timestamp);
+        $(this.id + ' span.statusInWords').html(statusInWords);
+        var changeSetComment = status.changeSet.items.length > 0 ? status.changeSet.items[0].comment : "Missing Comment!";
+        $(this.id + " span.changeSetComment").html(changeSetComment.substring(0, 140));
+        var claim = getClaimObject(status);
+        if(claim)
+        {
+          $(this.id + " span.claim").html("Claimed by " + claim.claimedBy + " because " + claim.reason);
+        }
+        if(!claim) {
+          $(this.id + " span.claim").css('display', 'hidden');
+        }
+      }
+    });
   });
 }
 
 function hudsonFull() {
   $('.hudson').each(function () {
     GM_xmlhttpRequest({
-     method: 'GET',
-     url: $('#' + $(this).attr('id') + ' a').attr('href') + 'api/json',
-     baseUrl: $('#' + $(this).attr('id') + ' a').attr('href'),
-     id: '#' + $(this).attr('id'),
-     onload: function(response) {
-	    var status = JSON.parse(response.responseText);
-		if(status.healthReport) {
-			$(this.id + ' span.healthScore').html( status.healthReport[0].score );
-			$(this.id + ' span.healthScore').css("opacity", (status.healthReport[0].score / 100 ));
-			$(this.id + ' span.healthScore').css("font-size", $(this.id + ' span.statusInWords').css("font-size") );
-		}
-	 }
-   });
+      method: 'GET',
+      url: $('#' + $(this).attr('id') + ' a').attr('href') + 'api/json',
+      baseUrl: $('#' + $(this).attr('id') + ' a').attr('href'),
+      id: '#' + $(this).attr('id'),
+      onload: function(response) {
+        var status = JSON.parse(response.responseText);
+        if(status.healthReport) {
+          $(this.id + ' span.healthScore').html( status.healthReport[0].score );
+          $(this.id + ' span.healthScore').css("opacity", (status.healthReport[0].score / 100 ));
+          $(this.id + ' span.healthScore').css("font-size", $(this.id + ' span.statusInWords').css("font-size") );
+        }
+      }
+    });
   });
 }
 
 function updateClass(status, id) {
-	if (status.building) {
-		if(!id.hasClass('building')) {
-			appendBuilding(id);
-		}
-		return;
-	}
-	clearClasses(id, status);
-    id.addClass(classToUpdate(status));	
-	return;
+  if (status.building) {
+    if(!id.hasClass('building')) {
+      appendBuilding(id);
+    }
+    return;
+  }
+  clearClasses(id, status);
+  id.addClass(classToUpdate(status)); 
+  return;
 }
+
 function appendBuilding(id) {
-	id.addClass('building');
-	
+  id.addClass('building');
 }
 
 function classToUpdate(status, url) {
-	if (isSuccess(status)) {
-        return 'success';
-    } else {
-        return 'failure';
-    }
+  if (isSuccess(status)) {
+    return 'success';
+  } else {
+    return 'failure';
+  }
 }
 
 function clearClasses(id, status) {
@@ -203,11 +135,11 @@ function isSuccess(status) {
 
 function message(status) {
   if (status.building) {
-      return ' started building';
+    return ' started building';
   } else if (isSuccess(status)) {
-      return ' passed';
+    return ' passed';
   } else {
-      return numberOfFailures(status) + ' failed';
+    return numberOfFailures(status) + ' failed';
   }
 }
 
@@ -215,28 +147,21 @@ function numberOfFailures(status) {
   var failCount = '0';
   $.each(status.actions, function(i,item){
               if (item.failCount){
-           failCount = item.failCount;
-    }
+           			failCount = item.failCount;
+    					}
   });
   return failCount;
 }
 
 function duration(status, idPrefix) {
   if (status.building) {
-      return '';
+    return '';
   }
   var duration = Math.round((status.duration / 1000 / 60));
   return 'after ' + duration + ' minutes &nbsp;-&nbsp;';
-
-}
-
-function userFeedback() {
-  $('.userFeedback').each(function () {
-  });
 }
 
 function differentialTime(date) {
-
   timezoneFix = 0 * 60 * 60 * 1000;
   now = new Date()
   diff = now - date + timezoneFix
@@ -251,27 +176,27 @@ function differentialTime(date) {
     diff = diff - days * millisecondsInDay
   }
   if (diff > millisecondsInHour) {
-      hours = Math.floor(diff / millisecondsInHour)
-      diff = diff - hours * millisecondsInHour
+    hours = Math.floor(diff / millisecondsInHour)
+    diff = diff - hours * millisecondsInHour
   }
   if (diff > millisecondsInMinute) {
-      minutes = Math.floor(diff / millisecondsInMinute)
+    minutes = Math.floor(diff / millisecondsInMinute)
   }
   var s = ""
   if (days > 0) {
-      s = ", " + days + " day" + (days > 1 ? "s" : "")
+    s = ", " + days + " day" + (days > 1 ? "s" : "")
   }
   if (hours > 0) {
-      s += ", " + hours + " hour" + (hours > 1 ? "s" : "")
+    s += ", " + hours + " hour" + (hours > 1 ? "s" : "")
   }
   if (minutes > 0 && days == 0) {
-      //s += ", " + minutes + " minutes" + (minutes>1 ? "s" : "")
-      s += ", " + minutes + " min"
+    //s += ", " + minutes + " minutes" + (minutes>1 ? "s" : "")
+    s += ", " + minutes + " min"
   }
   if (s == "") {
-      s = "less than 1 minute ago"
+    s = "less than 1 minute ago"
   } else {
-      s = s.substring(2) + " ago"
+    s = s.substring(2) + " ago"
   }
   return s;
 }
