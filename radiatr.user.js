fbc0bd22e561c8ce93cd257990cc78c43e96b6a6
Derek/Gabriel <derek.r.hammer@gmail.com>
2011-02-14 18:34:21 -0800

    Adding pulse and larger widths.

--- radiatr.user.js
+++ radiatr.user.js
@@ -5,17 +5,56 @@
 // @require   http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
 // @require   http://ajax.googleapis.com/ajax/libs/jqueryui/1.5.2/jquery-ui.min.js
 // @require   http://timeago.yarp.com/jquery.timeago.js
-// @require   jquery.fitTextToHeight.js
 
 // ==/UserScript==
 //
 
 refresh();
 
+(function( $, undefined ) {
+
+$.effects.pulsate = function(o) {
+	return this.queue(function() {
+		var elem = $(this),
+			mode = $.effects.setMode(elem, o.options.mode || 'show');
+			times = ((o.options.times || 5) * 2) - 1;
+			duration = o.duration ? o.duration / 2 : $.fx.speeds._default / 2,
+			isVisible = elem.is(':visible'),
+			opacity = o.options.opacity || 0,
+			animateTo = 0;
+
+		if (!isVisible) {
+			elem.css('opacity', opacity).show();
+			animateTo = 1;
+		}
+
+		if ((mode == 'hide' && isVisible) || (mode == 'show' && !isVisible)) {
+			times--;
+		}
+
+		for (var i = 0; i < times; i++) {
+			elem.animate({ opacity: animateTo === 1 ? 1 : opacity }, duration, o.options.easing);
+			animateTo = (animateTo + 1) % 2;
+		}
+
+		elem.animate({ opacity: animateTo === 1 ? 1 : opacity }, duration, o.options.easing, function() {
+			(o.callback && o.callback.apply(this, arguments));
+		});
+
+		elem
+			.queue('fx', function() { elem.dequeue(); })
+			.dequeue();
+	});
+};
+
+})(jQuery);
+
+
 function refresh() {
   ping();
   hudson();
   hudsonFull();
+  $('.building').filter(':not(:animated)').effect("pulsate", {times: 1, opacity: 0.5}, 2000);
   setTimeout(refresh, 3000);
 }
 
