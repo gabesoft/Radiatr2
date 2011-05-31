8899ee4489e6779f7f49396a40742b347180144a
Derek Hammer <derek.r.hammer@gmail.com>
2011-05-30 20:13:15 -0500

    Draw grid based on data from sinatra.

--- public/javascripts/board.coffee
+++ public/javascripts/board.coffee
@@ -1,8 +1,24 @@
+getStatusClass = (build) ->
+  result = if build.status == 'Fail' then 'fail' else 'success'
+  result += ' building' if build.building
+  result
+
+createBuildRow = (build) ->
+  result = "<tr class='" + getStatusClass(build) + "'>"
+  result += "<td>" + build.job + "</td>"
+  result += "<td>" + build.health + "</td>"
+  result += "<td>" + build.project + "</td>"
+  result += "<td>" + build.committers + "</td>"
+  result += "</tr>"
+
+populateGrid = (data) ->
+  for build in JSON.parse(data).builds
+    $('#grid').append createBuildRow build
+
 $(document).ready ->
 #  $('.building').filter(':not(:animated)').effect('pulsate', { times: 1, opacity: 0.5 }, 2000)
   $.ajax
     method: 'GET'
     url: '/builds'
-    success: (data) ->
-      console.log data
+    success: populateGrid
 
--- public/javascripts/board.js
+++ public/javascripts/board.js
@@ -1,11 +1,37 @@
 (function() {
+  var createBuildRow, getStatusClass, populateGrid;
+  getStatusClass = function(build) {
+    var result;
+    result = build.status === 'Fail' ? 'fail' : 'success';
+    if (build.building) {
+      result += ' building';
+    }
+    return result;
+  };
+  createBuildRow = function(build) {
+    var result;
+    result = "<tr class='" + getStatusClass(build) + "'>";
+    result += "<td>" + build.job + "</td>";
+    result += "<td>" + build.health + "</td>";
+    result += "<td>" + build.project + "</td>";
+    result += "<td>" + build.committers + "</td>";
+    return result += "</tr>";
+  };
+  populateGrid = function(data) {
+    var build, _i, _len, _ref, _results;
+    _ref = JSON.parse(data).builds;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      build = _ref[_i];
+      _results.push($('#grid').append(createBuildRow(build)));
+    }
+    return _results;
+  };
   $(document).ready(function() {
     return $.ajax({
       method: 'GET',
       url: '/builds',
-      success: function(data) {
-        return console.log(data);
-      }
+      success: populateGrid
     });
   });
 }).call(this);
